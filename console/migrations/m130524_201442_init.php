0c26a51dc653705aa7fb79c9f3b115919a59a546
Denis Pugachev <xdpugachevx (at) gmail (dot) com>
2017-02-21 23:07:47 +0400

    Init migration

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -2,6 +2,9 @@
 
 use yii\db\Migration;
 
+/**
+ * Migration creates tables: 'user', 'country', 'region', 'city'.
+ */
 class m130524_201442_init extends Migration
 {
     /** {@inheritDoc} */
@@ -10,14 +13,36 @@ class m130524_201442_init extends Migration
         $this->createTable('user', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
+            'access_token' => $this->string(32)->notNull()->unique(),
         ]);
+
+        $this->createTable('country', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+        ]);
+
+        $this->createTable('region', [
+            'id' => $this->primaryKey(),
+            'country_id' => $this->integer()->notNull(),
+            'name' => $this->string()->notNull(),
+        ]);
+        $this->createIndex('region_country_id', 'region', 'country_id');
+
+        $this->createTable('city', [
+            'id' => $this->primaryKey(),
+            'region_id' => $this->integer()->notNull(),
+            'name' => $this->string()->notNull(),
+        ]);
+        $this->createIndex('city_region_id_name', 'city', ['region_id', 'name'], true);
     }
 
     /** {@inheritDoc} */
     public function down()
     {
+        $this->dropTable('city');
+        $this->dropTable('region');
+        $this->dropTable('country');
         $this->dropTable('user');
     }
 }
