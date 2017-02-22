a4d0bb63529be6658bd302589c3cbee81445302f
Denis Pugachev <xdpugachevx (at) gmail (dot) com>
2017-02-22 19:46:30 +0400

    Added content generation command to provision script

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -33,6 +33,9 @@ info "Init project"
 info "Apply migrations"
 ./yii migrate <<< "yes"
 
+info "Create content data"
+./yii create-content
+
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
 
